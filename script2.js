aa4ee14097d9267360e70a4adb96597c530f4bf5
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-03-18 11:06:17 -0300

    diminuir e aumentar fonte

--- script2.js
+++ script2.js
@@ -7,6 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-})
+
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    })
+
+
 
 
