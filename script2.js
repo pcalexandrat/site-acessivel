c6630d3ca31a3f404f315445e2f23bbd06870205
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-03-18 11:26:41 -0300

    coment

--- script2.js
+++ script2.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     let tamanhoAtualFonte = 1;
-
+    
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-
     })
 
 
