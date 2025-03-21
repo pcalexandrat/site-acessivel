addd5185a7c5cb17dbad7738dc7996696dd4e90e
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-03-20 21:04:12 -0300

    crase

--- script2.js
+++ script2.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
