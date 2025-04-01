b8e995300ed6a9198809d12fe25fb868931d134f
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-04-01 10:29:39 -0300

    .

--- script2.js
+++ script2.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
+
     let tamanhoAtualFonte = 1;
     
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
