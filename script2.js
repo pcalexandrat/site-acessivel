6e88b8647a54a00bdc2c8002b933801fee9b93be
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-04-08 10:44:55 -0300

    criei button contraste

--- script2.js
+++ script2.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
-
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-
     botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
